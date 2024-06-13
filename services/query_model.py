16bdc32a0ee7dd23cd1e6388cc37f74e06374251
hamza08003 <hamza2082003@gmail.com>
2024-06-13 20:11:19 +0500

    Added support for Gemini model and refactored the model query functions

--- services/query_model.py
+++ services/query_model.py
@@ -1,9 +1,25 @@
 from langchain_openai import OpenAI
+import google.generativeai as genai
 from utils.config import OPENAI_API_KEY
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=GOOGLE_API_KEY)
+
+
+# Function to query the Gemini model given the prompt
+def query_gemini_model(prompt):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content(prompt)
+    return response.candidates[0].content.parts[0].text
 
 
 # Function to query the model with the question related to the data
-def query_model(prompt):
+def query_gpt_model(prompt):
     openai_llm = OpenAI(api_key=OPENAI_API_KEY)
     response = openai_llm.invoke(prompt)
     return response
